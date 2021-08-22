4e5d03bc385e39a76b033227bf1a8347e5f01c49
Lukas Voigt <stu217908@mail.uni-kiel.de>
2021-08-22 06:46:45 -0700

    added ICouplingAnalyzer

---
+++ coupling/ICouplingAnalyser.hpp
@@ -0,0 +1,13 @@
+#ifndef STATICCOUPLINGANALYZER_ICOUPLINGANALYSER_H
+#define STATICCOUPLINGANALYZER_ICOUPLINGANALYSER_H
+
+#include "AbstractCoupling.hpp"
+
+namespace coupling
+{
+class ICouplingAnalyser()
+{
+    virtual void handleCoupling(AbstractCoupling * coupling) = 0;
+}
+}  // namespace coupling
+#endif  // STATICCOUPLINGANALYZER_ICOUPLINGANALYSER_H
