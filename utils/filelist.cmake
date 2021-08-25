adaa61d74d51e1ba584a4c38ca4a35a5337dda12
Lukas Voigt <stu217908@mail.uni-kiel.de>
2021-08-25 02:16:38 -0700

    added utils

---
+++ utils/filelist.cmake
@@ -0,0 +1,12 @@
+## Add include folders
+LIST(APPEND UTILS_INCLUDEDIRS
+        "${CMAKE_CURRENT_LIST_DIR}"
+        )
+
+## Add source files
+LIST(APPEND UTILS_SOURCE_FILES
+        #"${CMAKE_CURRENT_LIST_DIR}/foo.cpp"
+        )
+
+## Add subdirectories
+#INCLUDE("${CMAKE_CURRENT_LIST_DIR}/SUBDIR/filelist.cmake")
