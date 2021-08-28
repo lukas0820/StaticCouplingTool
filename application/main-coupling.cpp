bef6e213f28f4b1041cfcdcdf5cf6622a9749815
Lukas Voigt <stu217908@mail.uni-kiel.de>
2021-08-28 04:10:07 -0700

    use application class in main function

--- application/main-coupling.cpp
+++ application/main-coupling.cpp
@@ -2,12 +2,13 @@
 #include <clang/Tooling/Tooling.h>
 #include <llvm/Support/CommandLine.h>
 
-#include <fstream>
-#include <typeinfo>
-
+#include "ClangCouplingApplication.h"
 #include "ClangCouplingFinder.hpp"
 #include "CommandLineExporter.hpp"
+#include "ConfigurationManager.h"
 #include "FileCouplingAnalyser.hpp"
+#include "FileUtils.hpp"
+
 
 using std::string;
 
@@ -18,34 +19,15 @@ using coupling::FileCouplingAnalyser;
 using language::cpp::ClangCouplingFinder;
 
 
-static llvm::cl::OptionCategory category("test");
-
 int main(int argc, const char** argv)
 {
-    ClangCouplingFinder couplingFinder;
-    ClangCouplingFinder::InitStatus status =
-        couplingFinder.init("/home/lukas/Documents/static-coupling/cmake-build-debug");
-    std::vector<std::string> fileList = couplingFinder.getCompilationDBFiles();
+    application::ConfigurationManager::setArguments(argc, argv);
+    std::string language = application::ConfigurationManager::getInstance()->getOptionValue("language");
 
-    CommandLineExporter exporter;
+    coupling::CommandLineExporter exporter;
+    application::ClangCouplingApplication application(&exporter);
 
-    FileCouplingAnalyser analyser;
-    analyser.setResultExporter(&exporter);
-    analyser.init(fileList);
-
-
-    std::function<void(AbstractCoupling*)> analyserCallback = [&analyser](AbstractCoupling* c)
-    { analyser.handleCoupling(c); };
-
-
-    couplingFinder.registerCouplingCallback(coupling::FileCoupling::getCouplingClassTypeId(), analyserCallback);
-
-
-    if (status == ClangCouplingFinder::InitStatus::OK)
-    {
-        couplingFinder.execute();
-        analyser.finish();
-    }
+    application.execute();
 
     return 0;
 }
