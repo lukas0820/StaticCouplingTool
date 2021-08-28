9eda530d972c435d75042af91ff059f59a662f27
Lukas Voigt <stu217908@mail.uni-kiel.de>
2021-08-28 04:08:49 -0700

    update cmake configuration

--- application/filelist.cmake
+++ application/filelist.cmake
@@ -1,4 +1,6 @@
 ## Add include folders
+
+
 LIST(APPEND APPLICATION_INCLUDEDIRS
         "${CMAKE_CURRENT_LIST_DIR}"
         )
@@ -6,7 +8,14 @@ LIST(APPEND APPLICATION_INCLUDEDIRS
 ## Add source files
 LIST(APPEND APPLICATION_SOURCE_FILES
         "${CMAKE_CURRENT_LIST_DIR}/main-coupling.cpp"
+        "${CMAKE_CURRENT_LIST_DIR}/CLIParser.cpp"
+        "${CMAKE_CURRENT_LIST_DIR}/ConfigurationManager.cpp"
+        "${CMAKE_CURRENT_LIST_DIR}/ConfigurationArgumentDatabase.cpp"
+        "${CMAKE_CURRENT_LIST_DIR}/JSONParser.cpp"
+        "${CMAKE_CURRENT_LIST_DIR}/AbstractCouplingApplication.cpp"
+        "${CMAKE_CURRENT_LIST_DIR}/ClangCouplingApplication.cpp"
         )
+
 
 ## Add subdirectories
 #INCLUDE("${CMAKE_CURRENT_LIST_DIR}/SUBDIR/filelist.cmake")
